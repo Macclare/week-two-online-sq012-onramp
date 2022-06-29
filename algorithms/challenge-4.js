70a13eb1af9bd09cbbd231653419ac45bfba21af
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-29 16:34:13 +0100

    Update challenge-4.js
    first commit

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,5 +1,14 @@
 function replaceDivisible(x, y) {
-    
-}
+ let newArr = [];
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] % y === 0) {
+      newArr.push("isDivisible");
+    } else {
+      newArr.push(x[i]);
+    }
+  }
+  return newArr;
+}   
 
 module.exports = replaceDivisible
+console.log(replaceDivisible([2, 4, 3, 6, 5], 3));
