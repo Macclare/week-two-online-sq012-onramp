041eec7c3d17cf0c15800cf0174bdee6ab1987b1
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-29 11:50:01 +0100

    Update challenge-3.js
    first commit

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,13 @@
 function missingNumber(numArr) {}
-
-module.exports = missingNumber
+ let missing = [];
+  for (let i = 1; i <= 9; i++) {
+    if (!numArr.includes(i)) {
+      missing.push(i);
+    }
+  }
+  if (missing.length < 1) return false;
+  if (missing.length === 1) return missing[0];
+  return missing;
+}
+module.exports = missingNumber;
+console.log(missingNumber([1, 2, 3, 4, 6, 7, 8, 9]));
