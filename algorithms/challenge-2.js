6d92987d50424c377767346829ef975589c8c6e8
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-29 11:49:11 +0100

    Update challenge-2.js
    first commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,9 @@
 function checkPrime(num) {
-  
+  let prime1 = num;
+  for (let i = 2; i * i <= prime1; i++) if (prime1 % i === 0) return false;
+  return prime1 > 1;
 }
 
-module.exports = checkPrime
+module.exports = checkPrime;
+
+console.log(checkPrime(2)); 
