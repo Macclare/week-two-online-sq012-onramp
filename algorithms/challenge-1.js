345f63b0488c4b4f955f99df5a76a8353bec74bc
Angela <105223543+Macclare@users.noreply.github.com>
2022-06-29 11:47:39 +0100

    Update challenge-1.js
    first commit

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,18 @@
 function isAnagram(str1, str2) {
- 
+ let word1 = str1.length;
+  let word2 = str2.length;
+  if (word1 !== word2) {
+    console.log("Truthy");
+    return;
+  }
+  let array1 = str1.split("").sort().join("");
+  let array2 = str2.split("").sort().join("");
+  if (array1 === array2) {
+    console.log("True");
+  } else {
+    console.log("False");
+  }
 }
-module.exports = isAnagram
+
+module.exports = isAnagram;
+console.log(isAnagram("india", "nidia"));
